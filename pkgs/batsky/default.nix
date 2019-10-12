a5ae145df66c1c6fc3315f66d08a92c466fad78e
Olivier Richard <olivier.richard@imag.fr>
2019-10-12 10:21:51 +0200

    Pin batsky version

--- pkgs/batsky/default.nix
+++ pkgs/batsky/default.nix
@@ -1,25 +1,21 @@
-{ stdenv, fetchgit, python37Packages, pkgs, zeromq }:
+{ stdenv, fetchgit, fetchFromGitHub, python37Packages, pkgs, zeromq }:
 
 python37Packages.buildPythonApplication rec {
   pname = "batsky";
   version = "0.1.0";
   
-  src = fetchgit {
-    url = /home/auguste/dev/batsky;
-    # echo controller
-    #sha256 = "00jbsb140b1662akzzrlm3pd8g3jnjmf1276rxzl6k7x788hh0m5";
-    #rev = "c216ea3ea3d10e244c651dfaad7649f56d7c9e7f";
-    # fast2
-    sha256 = "17lakcbdfpwi6d8648cb7x6hmm0vylry2336zb901fl04h7d5l75";
-    rev = "d18cac7666fdea9d07383fca2097dc06c6c079b5";
+  #src = fetchgit {
+  #  url = /home/auguste/dev/batsky;
+  #  sha256 = "17lakcbdfpwi6d8648cb7x6hmm0vylry2336zb901fl04h7d5l75";
+  #  rev = "d18cac7666fdea9d07383fca2097dc06c6c079b5";
     
-  };
-  #src = fetchFromGitHub {
-  #  owner = "oar-team";
-  #  repo = "batsky";
-  #  rev = "";
-  #  sha256 = "";
   #};
+  src = fetchFromGitHub {
+    owner = "oar-team";
+    repo = "batsky";
+    rev = "d18cac7666fdea9d07383fca2097dc06c6c079b5";
+    sha256 = "17lakcbdfpwi6d8648cb7x6hmm0vylry2336zb901fl04h7d5l75";
+  };
 
   propagatedBuildInputs = with python37Packages; [
     click
