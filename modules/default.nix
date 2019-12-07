75e83bd16a77e2123d2123812e2333476b41d898
Olivier Richard <olivier.richard@imag.fr>
2019-12-07 17:45:12 +0100

    [oar] remove useless oar-api file

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,6 @@
   bs-slurm = ./services/bs-slurm.nix;
   fe-slurm = ./services/fe-slurm.nix;
   rs-munge = ./services/bs-munge.nix;
-  oar-service = ./services/oar.nix;
-  oar-api = ./services/oar-api.nix;
+  oar = ./services/oar.nix;
 }
 
